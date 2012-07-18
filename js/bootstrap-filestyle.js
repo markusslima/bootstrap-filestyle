099ab907c976c365cd03b039eb0f8a8856e25990
Moogles <markusslima@gmail.com>
2012-07-18 01:30:59 -0300

    corrections

--- js/bootstrap-filestyle.js
+++ js/bootstrap-filestyle.js
@@ -1,22 +1,39 @@
+/*
+ * bootstrap-filestyle.js v1.0
+ * http://markusslima.github.com/Filestyle-bootstrap/
+ *
+ * Copyright 2012
+ *
+ * Licensed under the MIT License
+ */
+
 (function ($) {
     // Register plugin
-    $.fn.filestyle = function (option) {
+    $.fn.filestyle = function (options) {
+		var defaults = {
+			buttonText : 'Choose file',
+			textField : true,
+			classButton : '',
+			classText : ''
+		}
+
+		var options = $.extend(defaults, options);
+
         return this.each(function () {
             var $this = $(this)
-           
             $this
-                .css({'position':'absolute','top':'-100px','left':'-100px'})
+                .css({'position':'fixed','top':'-100px','left':'-100px'})
                 .parent()
-                .addClass("filestyle-content form-search")
-                .append('<input type="text" disabled size="40" /> '+
-                    '<button class="btn btn-primary">Choose file</button>');
-            
+                .addClass("form-search")
+                .append((options.textField ? '<input type="text" class="'+options.classText+'" disabled size="40" /> ' : '')+
+                    '<input type="button" class="btn '+options.classButton+'" value="'+options.buttonText+'" />');
+
             $this.change(function () {
                 $this.parent().children(':text').val($(this).val());
             });
             
             // Add event click
-            $this.parent().children('button').click(function () {
+            $this.parent().children(':button').click(function () {
                 $this.click();
             });
         });
