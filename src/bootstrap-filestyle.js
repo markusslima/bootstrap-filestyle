33023528ce566214a6c4d2e46763b258dfc38177
Stefan Natchev <stefan.natchev@gmail.com>
2013-02-28 11:46:32 -0500

    whitespace conversion

--- src/bootstrap-filestyle.js
+++ src/bootstrap-filestyle.js
@@ -10,56 +10,55 @@
     "use strict";
     // Register plugin
     $.fn.filestyle = function (options) {
-		if (typeof options === 'object' || typeof options === 'undefined'){
-			var defaults = {
-					buttonText : 'Choose file',
-					textField : true,
-					icon : false,
-					classButton : '',
-					classText : '',
-					classIcon : 'icon-folder-open'
-				};
+    if (typeof options === 'object' || typeof options === 'undefined'){
+      var defaults = {
+          buttonText : 'Choose file',
+          textField : true,
+          icon : false,
+          classButton : '',
+          classText : '',
+          classIcon : 'icon-folder-open'
+        };
 
-			options = $.extend(defaults, options);
+      options = $.extend(defaults, options);
 
-			return this.each(function () {
-				var $this = $(this);
+      return this.each(function () {
+        var $this = $(this);
 
-				$this.data('filestyle', true);
+        $this.data('filestyle', true);
 
-				$this
-					.css({'position':'fixed','top':'-100px','left':'-100px'})
-					.find("input[type=file]")
-					.after(
-						(options.textField ? '<input type="text" class="'+options.classText+'" disabled size="40" /> ' : '')+
-						'<button type="button" class="btn '+options.classButton+'" >'+
-							(options.icon ? '<i class="'+options.classIcon+'"></i> ' : '')+
-							options.buttonText+
-						'</button>'
-					);
+        $this
+          .css({'position':'fixed','top':'-100px','left':'-100px'})
           .parents(".control-group")
             .addClass("input-append")
+            .find("input[type=file]")
+            .after(
+              (options.textField ? '<input type="text" class="'+options.classText+'" disabled size="40" /> ' : '')+
+                '<button type="button" class="btn '+options.classButton+'" >'+
+                (options.icon ? '<i class="'+options.classIcon+'"></i> ' : '')+
+                options.buttonText+
+              '</button>'
+            );
 
-				$this.change(function () {
-					$this.parent().children(':text').val($(this).val().split("\\").pop());
-				});
+        $this.change(function () {
+          $this.parent().children(':text').val($(this).val().split("\\").pop());
+        });
 
-				
-				// Add event click
-				$this.parent().children(':button').click(function () {
-					$this.click();
-				});
-			});
-		} else {
-			return this.each(function () {
-				var $this = $(this);
-				if ($this.data('filestyle') === true && options === 'clear') {
-					$this.parent().children(':text').val('');
-					$this.val('');
-				} else {
-					window.console.error('Method filestyle not defined!');
-				}
-			});
-		}
+        // Add event click
+        $this.parent().children(':button').click(function () {
+          $this.click();
+        });
+      });
+    } else {
+      return this.each(function () {
+        var $this = $(this);
+        if ($this.data('filestyle') === true && options === 'clear') {
+          $this.parent().children(':text').val('');
+          $this.val('');
+        } else {
+          window.console.error('Method filestyle not defined!');
+        }
+      });
+    }
     };
 }(jQuery));
