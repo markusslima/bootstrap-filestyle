22a9376d4ce46d22a6490e842cf7fd4b3c6ee349
Markus Lima <markusslima@gmail.com>
2017-08-16 15:33:18 -0300

    update page test

--- src/bootstrap-filestyle.js
+++ src/bootstrap-filestyle.js
@@ -4,7 +4,7 @@
  * github: https://github.com/markusslima/bootstrap-filestyle
  *
  * Copyright (c) 2017 Markus Vinicius da Silva Lima
- * Version 1.2.3
+ * Version 1.3.0
  * Licensed under the MIT license.
  */
 (function($) {
