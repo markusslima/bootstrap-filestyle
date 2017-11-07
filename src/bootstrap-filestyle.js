3943536cf4c9f9de3eee265b04da76eb3f026928
Markus Lima <markusslima@gmail.com>
2017-11-07 08:38:26 -0200

    Add dragdrop options and method Update version 2.1.0

--- src/bootstrap-filestyle.js
+++ src/bootstrap-filestyle.js
@@ -4,7 +4,7 @@
  * github: https://github.com/markusslima/bootstrap-filestyle
  *
  * Copyright (c) 2017 Markus Vinicius da Silva Lima
- * Version 2.0.0
+ * Version 2.1.0
  * Licensed under the MIT license.
  */
 (function($) {
